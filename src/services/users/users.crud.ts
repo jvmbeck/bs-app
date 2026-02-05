3e367abc774b47e42df10f023b442a4caef7e93c
jvmbeck <jmadridbeck@gmail.com>
2026-02-05 16:22:06 -0300

    added function to query active users

--- src/services/users/users.crud.ts
+++ src/services/users/users.crud.ts
@@ -1,6 +1,7 @@
 import { db } from 'src/key/configKey';
 import { doc, getDoc } from 'firebase/firestore';
 import type { UserModel } from 'src/models/FirestoreModels';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 
 export async function getUserById(uid: string): Promise<UserModel | null> {
   return await getDoc(doc(db, 'users', uid)).then((snap) => {
@@ -9,4 +10,20 @@ export async function getUserById(uid: string): Promise<UserModel | null> {
     }
     return null;
   });
+}
+
+export interface UserOptionDTO {
+  id: string;
+  name: string;
+}
+
+export async function fetchActiveUsersOptions(): Promise<UserOptionDTO[]> {
+  const q = query(collection(db, 'users'), where('active', '==', true));
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    name: doc.data().name,
+  }));
 }
